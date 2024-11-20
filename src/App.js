156626572be50006d7cfed7b64bb39181da43c10
chzhi <chzhi@paypal.com>
2024-11-20 14:31:51 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import Dashboard from './component/Dashboard';
-import Header from './component/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-        <Header />
-        <Dashboard />
+      hello
     </div>
   );
 }
